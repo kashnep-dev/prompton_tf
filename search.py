fd7b8dc24ef9e08a5e1b0b5617090acb356b22d3
JaeHyunEo <43471037+kashnep-dev@users.noreply.github.com>
2024-04-08 07:16:12 +0000

    dart 재무정보 호출함수 추가

--- search.py
+++ search.py
@@ -1,10 +1,20 @@
 import json
 import os
 import urllib.request
+from collections import OrderedDict
 from datetime import datetime, timedelta
 
 import requests
 from bs4 import BeautifulSoup
+
+
+def is_json_key_present(json, key):
+    try:
+        buf = json[key]
+    except KeyError:
+        return False
+
+    return True
 
 
 # bing 검색결과를 가져오는 함수
@@ -27,7 +37,7 @@ def search_by_bing(param):
     return url_list
 
 
-# bing 검색결과를 가져오는 함수
+# bing 검색 API 결과를 가져오는 함수
 def search_by_bing_api(param):
     subscription_key = os.getenv("BING_SEARCH_KEY")
     api_endpoint = os.getenv("BING_SEARCH_ENDPOINT")
@@ -69,15 +79,15 @@ def search_by_yna(param):
 
 # 네이버 뉴스 검색 API 
 def search_by_naver_api(param):
-    client_id = "zn8zLchSGS3_WhIMHNpY"
-    client_secret = "mhrP9vRyIk"
+    client_id = os.getenv("NAVER_CLIENT_ID")
+    client_secret = os.getenv("NAVER_CLIENT_SECRET")
 
     encText = urllib.parse.quote(param)
-    display_num = urllib.parse.quote("100")  # 한 번에 표시할 검색 결과 개수(기본값: 10, 최댓값: 100) 
+    display_num = urllib.parse.quote("10")  # 한 번에 표시할 검색 결과 개수(기본값: 10, 최댓값: 100) 
     start = urllib.parse.quote("1")  # 검색 시작 위치(기본값: 1, 최댓값: 1000)
     sort = urllib.parse.quote("date")  # 검색 결과 정렬 방법 - sim: 정확도순으로 내림차순 정렬 / - date: 날짜순으로 내림차순 정렬
 
-    url = "https://openapi.naver.com/v1/search/news.json?query=" + encText + "&display=" + display_num + "&start=" + start + "&sort=" + sort  # JSON 결과
+    url = os.getenv("NAVER_API_ENDPOINT").format(encText, display_num, start, sort)  # JSON 결과
 
     request = urllib.request.Request(url)
     request.add_header("X-Naver-Client-Id", client_id)
@@ -87,8 +97,69 @@ def search_by_naver_api(param):
 
     if (rescode == 200):
         response_body = response.read()
-        result = json.loads(response_body)
-        print(result)
-        return json.dumps(result, ensure_ascii=False)
+        json_str = json.loads(response_body)
+        result = OrderedDict()
+        article_list = []
+        for article in json_str['items']:
+            article_dict = OrderedDict()
+            article_dict['originallink'] = article['originallink']
+            article_dict['description'] = article['description']
+            article_dict['pubDate'] = article['pubDate']
+            article_list.append(article_dict)
+        result['items'] = article_list
+        return result
+    else:
+        print("Error Code:" + rescode)
+
+
+# dart 재무정보 검색 API
+def search_by_dart_api(param):
+    dart_api_key = os.getenv("DART_API_KEY")
+    with open('dart.json') as f:
+        darts = json.load(f)
+    corp_code = ''
+    for corp in darts['items']:
+        if param == 'LG':
+            corp_code = '00120021'
+        elif param in corp['corp_name']:
+            corp_code = corp['corp_code']
+    url = os.getenv("DART_API_ENDPOINT").format(dart_api_key, corp_code)  # JSON 결과
+
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+
+    if (rescode == 200):
+        response_body = response.read()
+        json_str = json.loads(response_body)
+        info_list = []
+        result = OrderedDict()
+        for el in json_str['list']:
+            if el['account_id'] == 'ifrs-full_Revenue' or el['account_id'] == 'ifrs-full_ProfitLoss' or el[
+                'account_id'] == 'dart_OperatingIncomeLoss':
+                info_dict = OrderedDict()
+                if is_json_key_present(el, 'sj_nm'):
+                    info_dict['rcept_no'] = el['rcept_no']
+                    info_dict['reprt_code'] = el['reprt_code']
+                    info_dict['bsns_year'] = el['bsns_year']
+                    info_dict['corp_code'] = el['corp_code']
+                    info_dict['sj_div'] = el['sj_div']
+                    info_dict['sj_nm'] = el['sj_nm']
+                    info_dict['account_id'] = el['account_id']
+                    info_dict['account_nm'] = el['account_nm']
+                    info_dict['account_detail'] = el['account_detail']
+                    info_dict['thstrm_nm'] = el['thstrm_nm']
+                    info_dict['thstrm_amount'] = el['thstrm_amount']
+                    # info_dict['thstrm_add_amount'] = el['thstrm_add_amount']
+                    info_dict['frmtrm_nm'] = el['frmtrm_nm']
+                    info_dict['frmtrm_amount'] = el['frmtrm_amount']
+                    info_dict['bfefrmtrm_nm'] = el['bfefrmtrm_nm']
+                    info_dict['bfefrmtrm_amount'] = el['bfefrmtrm_amount']
+                    info_dict['ord'] = el['ord']
+                    info_dict['currency'] = el['currency']
+
+                    info_list.append(info_dict)
+                    result['items'] = info_list
+        return json.dumps(result, ensure_ascii=False, indent='\t')
     else:
         print("Error Code:" + rescode)
