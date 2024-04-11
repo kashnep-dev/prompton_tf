f2cdfe94b086ab73601733b6b68ebdd18f5f78aa
JaeHyunEo <43471037+kashnep-dev@users.noreply.github.com>
2024-04-11 04:59:07 +0000

    langsmith 모니터링 관리 코드 정리

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 
+import streamlit as st
 from langchain_core.runnables import RunnableConfig
 from langchain_core.tracers import LangChainTracer
 from langchain_core.tracers.run_collector import RunCollectorCallbackHandler
 from langsmith import Client
-import streamlit as st
-# Customize if needed
-def configure_run():
+
+
+def ls_configure():
     client = Client()
-    ls_tracer = LangChainTracer(project_name=os.environ["LANGCHAIN_PROJECT"], client=client)
+    ls_tracer = LangChainTracer(project_name=os.getenv("LANGCHAIN_PROJECT"), client=client)
     run_collector = RunCollectorCallbackHandler()
     cfg = RunnableConfig()
     cfg["callbacks"] = [ls_tracer, run_collector]
