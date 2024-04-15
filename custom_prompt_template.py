5d537cff1b7ec20f730416e3794453b75f2ded0d
어재현 <jaehyuneo@gmail.com>
2024-04-15 18:48:04 +0900

    프롬프트 수정

--- custom_prompt_template.py
+++ custom_prompt_template.py
@@ -25,41 +25,40 @@ class CustomPromptTemplate(Enum):
     <glossary>
         You are an expert in financial statement analysis who acts as an accountant and manages finances. When providing financial information for customers, you must properly analyze and understand financial statements. In some cases, you will need to check the income statement within the financial statements to provide an accurate answer.
         The provided document is a financial statement for {{company}} in Json format.
-        재무제표는 대차대조표(재무상태표), 손익계산서, 현금흐름표로 나타내야해.
-        대차대조표(재무상태표)를 통해 기업의 자본 및 부채 비율을 확인할 수 있어.
-        자산총계 = 부채총계 + 자본총계
-        손익계산서는 수주(매출액), 영업이익, 당기순이익(손실)로 확인할 수 있어.
-        LG헬로비전과 LG생활건강은 1원 단위, 나머지 회사는 백만원 단위야.
+           재무제표는 대차대조표(재무상태표), 손익계산서, 현금흐름표로 나타내야해.
+           대차대조표(재무상태표)를 통해 기업의 자본 및 부채 비율을 확인할 수 있어.
+           자산총계 = 부채총계 + 자본총계
+           손익계산서는 수주(매출액), 영업이익, 당기순이익(손실)로 확인할 수 있어.
+           LG헬로비전과 LG생활건강은 원 단위야.
+           LG라고 하면 {{corp_code}}가 00120021로 해줘.
         아래 텍스트에 언급된 단어는 모두 동일한 의미로 이해하면 돼.
-        <words>
-            재무정보
-            재무제표 현황
-            재무재표
-            재무제표
-            재무제표 정보
-            자산현황
-            매출현황
-            매출
-            매출정보
-            자산가치
-            실적
-        </words>
+         <words>
+          재무정보
+          재무제표 현황
+          재무재표
+          재무제표
+          재무제표 정보
+          자산현황
+          매출현황
+          매출
+          매출정보
+          자산가치
+          실적
+         </words>
     </glossary>
-    <json>
-        {context}
-    </json>
     <instruction>
-        위의 json은 특정회사의 재무정보입니다.
+        위에 json은 특정회사에 재무정보입니다.
         재무정보를 요약할때는 매출연도, 수주(매출액), 영업이익, 당기순이익을 알려줘.
         입력받은 {{thstrm_nm}} 값은 최근 3년치 정보입니다.
         수주(매출액) = 매출액 = 영업수익 = 매출및지분법손익 = 매출
         영업이익 = 영업이익(손실)
         당기순이익(손실) = 당기순이익
         {{thstrm_nm}} 값을 이용하여 최근 3년치 정보를 표로 생성해줘.
-        LG헬로비전과 LG생활건강은 1원 단위, 나머지 회사는 백만원 단위야.
-        표내에 금액은 백만단위로 표시해줘.
+        금액 단위는 백만원이야.
         표 이후에는 최근 1년치에 대한 매출연도, 수주(매출액), 영업이익, 당기순이익을 말로써 설명해줘.
-        #매출연도는 2023년 형식으로 표기해줘
+        ###
+        매출연도는 2023년 형식으로 표기해줘
+        ###
     </instruction>
     """
     STOCK_INFO_TEMPLATE = """
